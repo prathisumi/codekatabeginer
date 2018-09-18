d8cdf9be60c860dcc63ac9f20b2a19721ab2a3b0
prathisumi <43338077+prathisumi@users.noreply.github.com>
2018-09-18 12:43:16 +0530

    Update char.py

--- char.py
+++ char.py
@@ -3,7 +3,7 @@ if((c==a)or(c==e)or(c==i)or(c==o)or(c==u)):
   print("Vowel")
 else :
   if((c<0)or(c>0)or(c==0)):
-    print("Invalid input")
+    print("invalid")
   else:
     print("Consonant")
 
