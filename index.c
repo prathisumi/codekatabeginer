af2e219f1ec3d50cbbb95776073c0dc71ebce82e
prathisumi <43338077+prathisumi@users.noreply.github.com>
2019-03-05 12:01:57 +0530

    Create index.c

---
+++ index.c
@@ -0,0 +1,15 @@
+#include<stdio.h.
+#include<conio.h.
+void main()
+{
+int n,i,a[500];
+for (i=0:i<n;i++)
+{
+scanf("%d\t",&a[i]);
+}
+for (i=0;i<n;i++)
+{
+printf("%d\t%d\n",a[i],i);
+}
+getch();
+}
